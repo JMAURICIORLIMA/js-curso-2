c72b5dfaf7ed2f527f9e0819efea59e93b658e26
José Maurício <mauricio.rodriguesdelima@gmail.com>
2024-01-23 18:53:54 -0300

    Desafio 04

---
+++ Desafios/desafio04/desafio04.js
@@ -0,0 +1,23 @@
+// Desafio - 1
+let listaGenerica = [];
+
+
+// Desafio - 2
+let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+
+// Desafio - 3
+linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
+
+
+// Desafio - 4
+let listaDeElementos = ["Nome 1", "Nome 2", "Nome 3"];
+console.log(listaDeElementos[0]);
+
+
+// Desafio - 5
+console.log(listaDeElementos[1]);
+
+
+// Desafio - 6
+console.log(listaDeElementos[2]);
